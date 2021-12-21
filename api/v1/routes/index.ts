7bc9f819cde8be166d0d9fbb0a4313707ac0bb40
alighamdan <aligame9070@gmail.com>
2021-12-21 17:13:32 +0300

    first commit

--- api/v1/routes/index.ts
+++ api/v1/routes/index.ts
@@ -1,50 +1,8 @@
 import { Router } from "express";
 import { readdirs } from "../../../src/functions";
-// import tabnine from "./code/tabnine";
-// import stackoverflow from "./code/stackoverflow";
-// import githubgist from "./code/gistgithub";
-// import code from "./code/code";
-// import prettie from "./code/prettie";
-// import image from "./reads/image";
-// import tts from "./reads/tts";
-// import alternative from "./reads/alternative";
-// import qrRead from "./reads/readQR";
-// import isvirus from "./reads/isvirus";
-// import anime from "./search/anime";
-// import question from "./search/question";
-// import wiki from "./search/wiki";
-// import weather from "./search/weather";
-// import createBin from "./write/create";
-// import deleteBin from "./write/delete";
-// import searchBin from "./write/search";
-// import GetBin from "./write/get";
-// import chatbot from "./ai/chatbot";
-// import snippet from './ai/snippet';
-// import lyrics from './search/lyrics';
 
 const router = Router();
 
-// router.use("/code", tabnine);
-// router.use("/code", githubgist);
-// router.use("/code", stackoverflow);
-// router.use("/code", code);
-// router.use("/code", prettie);
-// router.use("/read", image);
-// router.use("/read", tts);
-// router.use("/read", alternative);
-// router.use("/read", qrRead);
-// router.use("/read", isvirus);
-// router.use("/search", anime);
-// router.use("/search", question);
-// router.use("/search", wiki);
-// router.use("/search", weather);
-// router.use('/search', lyrics)
-// router.use("/bin", createBin);
-// router.use("/bin", deleteBin);
-// router.use("/bin", searchBin);
-// router.use("/bin", GetBin);
-// router.use("/ai", chatbot);
-// router.use("/ai", snippet);
 
 readdirs(__dirname, ".ts")
   .filter((e) => !e.includes("index.ts"))
