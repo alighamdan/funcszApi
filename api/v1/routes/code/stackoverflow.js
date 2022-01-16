b4e9d04594bbd9765122f6cf9d21f0d3c0d357a0
ali ghamdan <73795044+alighamdan@users.noreply.github.com>
2022-01-16 22:27:37 +0300

    Update stackoverflow.js

--- api/v1/routes/code/stackoverflow.js
+++ api/v1/routes/code/stackoverflow.js
@@ -5,20 +5,14 @@ var functions_1 = require("./functions");
 var router = (0, express_1.Router)();
 router.get('/stackoverflow', function (req, res) {
     var q = req.query.q;
-    var limit = req.query.limit;
     if (!q)
         return res.status(404).json({ msg: "Please Provide q Param" });
     var start = Date.now();
     (0, functions_1.stackoverflowCodes)(String(q))
         .then(function (codes) {
-        console.log(codes);
         var endtime = Date.now();
         var elapsed = endtime - start;
-        if (limit && !isNaN(Number(limit))) {
-            res.json({ elapsedTimeInMS: elapsed, codes: codes.slice(0, Number(limit)) });
-        }
-        else
-            res.json({ elapsedTimeInMS: elapsed, codes: codes });
+        res.json({ elapsedTimeInMS: elapsed, codes: codes });
     })
         .catch(function (err) {
         res.status(500).json(err);
